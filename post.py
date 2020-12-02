a48c135dc697e54f002776bb7d82f0d9e12b4e0d
Filip Simek <filip.simek@gmail.com>
2020-12-02 12:15:58 +0000

    Add post generator

---
+++ post.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3.8
+
+import datetime
+import os
+import re
+
+STORY = 'spaceboy'
+BASE_DATE = datetime.date(2020, 11, 15)
+
+pics = os.listdir(STORY)
+for pic in pics:
+    m = re.search(r'(\d+) - (.*)\.png', pic)
+    if m is None:
+        continue
+    date = BASE_DATE + datetime.timedelta(days=(int(m.group(1)) - 1))
+    title = m.group(2)
+    fname = f"_posts/{date.strftime('%Y-%m-%d')}-{title}.md"
+    title = title[0].upper() + title[1:]
+
+    with open(fname, 'wt') as f:
+        print(f"""---
+layout: post
+title:  "{title}"
+date:   {date.strftime('%Y-%m-%d')} 00:00:00 +0000
+categories: {STORY}
+---
+
+![{title}](../{STORY}/{pic.replace(' ', '%20')})
+""",
+            file=f)
+
