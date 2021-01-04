f9564d418092b9a49ee6b0f5e62f9dc0f56a4b60
Filip Simek <filip.simek@gmail.com>
2021-01-04 00:13:05 +0000

    Add support for 'unsorted' story to post.py

--- post.py
+++ post.py
@@ -4,29 +4,59 @@ import datetime
 import os
 import re
 
-STORY = 'spaceboy'
-BASE_DATE = datetime.date(2020, 11, 15)
+BASE_DATES = {
+    'spaceboy': datetime.date(2020, 11, 15),
+    'unsorted': datetime.date(2020, 11, 15)
+}
+STORIES = list(BASE_DATES.keys())
 
-pics = os.listdir(STORY)
-for pic in pics:
-    m = re.search(r'(\d+) - (.*)\.png', pic)
-    if m is None:
-        continue
-    date = BASE_DATE + datetime.timedelta(days=(int(m.group(1)) - 1))
-    title = m.group(2)
-    fname = f"_posts/{date.strftime('%Y-%m-%d')}-{title}.md"
-    title = title[0].upper() + title[1:]
-    url = f"{STORY}/{pic.replace(' ', '%20')}"
+produced_posts = set()
 
-    with open(fname, 'wt') as f:
-        print(f"""---
+
+def make_story(story):
+    global produced_posts
+    pics = os.listdir(story)
+    for pic in pics:
+        m = re.search(r'(\d+) - (.*)\.png', pic)
+        if m is None:
+            continue
+
+        number = int(m.group(1))
+        if number > 1000:
+            # the number is a date: YYMMDD
+            year = number // 10000
+            month = (number // 100) % 100
+            day = number % 100
+            date = datetime.date(2000 + year, month, day)
+        else:
+            # number is the offset from base date (starting at 1 on the base date)
+            date = BASE_DATES[story] + datetime.timedelta(days=(number - 1))
+
+        title = m.group(2)
+
+        fname = f"_posts/{date.strftime('%Y-%m-%d')}-{title}.md"
+        while fname in produced_posts:
+            # When two different stories have the same word on the same day then
+            # we would have a clash - two posts with the same name - so we have to
+            # differentiate
+            fname = fname[:-3] + "_.md"
+        produced_posts.add(fname)
+
+        title = title[0].upper() + title[1:]
+        url = f"{story}/{pic.replace(' ', '%20')}"
+
+        with open(fname, 'wt') as f:
+            print(f"""---
 layout: post
 title:  "{title}"
 date:   {date.strftime('%Y-%m-%d')} 00:00:00 +0000
-categories: {STORY}
+categories: {story}
 ---
 
 [![{title}]({{{{ site.baseurl }}}}/{url})]({{{{ site.baseurl }}}}/{url})
 """,
-            file=f)
+                file=f)
 
+
+for story in STORIES:
+    make_story(story)
